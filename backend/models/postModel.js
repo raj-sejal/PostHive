6caf37fcf7281cfd621fe80558eb9cb4bd040530
Sejal Raj <rajsejalxxx@gmail.com>
2024-08-23 22:13:17 +0530

    Create postModel.js

---
+++ backend/models/postModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    code:{
+        type:String,
+        required:true
+    },
+    tags:{
+        type:[String],
+        required:true
+    },
+    author:{
+        type:String,
+        required:true
+    },
+    upvotes:{
+        type:Number,
+        default:0
+    },
+    email:{
+        type:String,
+        default:"CodeContribute@mail.com"
+    }
+   
+},{timestamps:true});
+
+module.exports = mongoose.model('post',postSchema);
